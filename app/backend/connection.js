96d133f20c125201edea73ced7cfc3c6faf1ab32
Jonas Schöley <jschoeley@gmail.com>
2026-01-30 10:46:52 +0100

    Fix Deprecated Mongoose Options

--- app/backend/connection.js
+++ app/backend/connection.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
 
 // Here, the second "mongodb" is the name of the service defined in docker-compose.yml file
-mongoose.connect('mongodb://mongodb:27017/mortalityDB1', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+mongoose.connect('mongodb://mongodb:27017/mortalityDB1');
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
