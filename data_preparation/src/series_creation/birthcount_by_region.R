843e66966f04a878f6737797dfa991a7cd5e652b
Jonas Schöley <jschoeley@gmail.com>
2026-02-14 17:06:26 +0100

    Add HFD Birthcount Series

---
+++ data_preparation/src/series_creation/birthcount_by_region.R
@@ -0,0 +1,55 @@
+# Create Series of birthcounts by region
+
+# Init --------------------------------------------------------------------
+
+library(yaml)
+library(qs)
+library(dplyr)
+library(tidyr)
+library(readr)
+
+# Constants ---------------------------------------------------------------
+
+# input and output paths
+setwd(".")
+paths <- list()
+paths$input <- list(
+  births_period_by_age.qs =
+    "./dat/hfd/births_period_by_age.qs"
+)
+paths$output <- list(
+  birthcount_by_region.csv =
+    "./out/series_creation/birthcount_by_region.csv"
+)
+
+# constants specific to this analysis
+cnst <- within(list(), {})
+
+# Load HMD data -----------------------------------------------------------
+
+hfd <- qread(paths$input$births_period_by_age.qs)
+
+# Create series -----------------------------------------------------------
+
+birthcount_by_region <-
+  bind_rows(hfd) |>
+  select(
+    z = Total,
+    x = Year,
+    y = Age,
+    region = isocode
+  ) |>
+  mutate(
+    z = as.integer(z),
+    x = as.integer(x),
+    y = as.integer(y)
+  )
+
+# Export ------------------------------------------------------------------
+
+# export results of analysis
+write_csv(
+  x = birthcount_by_region,
+  file = paths$output$birthcount_by_region.csv,
+  na = "."
+)
