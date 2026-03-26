98d30b0857280dde0e7c32e0ba33c089c5ea10f6
Jonas Schöley <jschoeley@gmail.com>
2026-03-26 13:28:02 +0100

    Add HFD Fertility Rates

---
+++ dataprep/src/series_creation/fertility_rate_by_region.R
@@ -0,0 +1,55 @@
+# Create Series of fertility rates by region
+
+# Init --------------------------------------------------------------------
+
+library(yaml)
+library(qs2)
+library(dplyr)
+library(tidyr)
+library(readr)
+
+# Constants ---------------------------------------------------------------
+
+# input and output paths
+setwd(".")
+paths <- list()
+paths$input <- list(
+  fertilityrate_period_by_age.qs =
+    "./dat/hfd/fertilityrates_period_by_age.qs"
+)
+paths$output <- list(
+  fertilityrate_by_region.csv =
+    "./out/series_creation/fertilityrate_by_region.csv"
+)
+
+# constants specific to this analysis
+cnst <- within(list(), {})
+
+# Load HFD data -----------------------------------------------------------
+
+hfd <- qs_read(paths$input$fertilityrate_period_by_age.qs)
+
+# Create series -----------------------------------------------------------
+
+fertilityrate_by_region <-
+  bind_rows(hfd) |>
+  select(
+    z = ASFR,
+    x = Year,
+    y = Age,
+    region = isocode
+  ) |>
+  mutate(
+    z = round(z, 6),
+    x = as.integer(x),
+    y = as.integer(y)
+  )
+
+# Export ------------------------------------------------------------------
+
+# export results of analysis
+write_csv(
+  x = fertilityrate_by_region,
+  file = paths$output$fertilityrate_by_region.csv,
+  na = "."
+)
